7fbcae6d846dc0d9cdeb594b4bb9db60b74ea2e9
Juan Antamba <juan.antamba@udla.edu.ec>
2023-12-03 02:47:37 -0500

    Se crea la funcion que servira pra imprimir la matriz

--- SumaMatrices.c
+++ SumaMatrices.c
@@ -10,3 +10,14 @@ void llenarMatrizAleatoria(int filas, int columnas, int matriz[filas][columnas])
         }
     }
 }
+
+// Creamos otra funcion que sera usa para imprimir la matriz
+void imprimirMatriz(int filas, int columnas, int matriz[filas][columnas]) {
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            printf("%d\t", matriz[i][j]); 
+        }
+        printf("\n"); 
+    }
+    printf("\n"); 
+}
