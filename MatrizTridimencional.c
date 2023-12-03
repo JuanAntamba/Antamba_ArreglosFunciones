a3ef8bb0a353079a0f0d7e5f290391e26a480f29
Juan Antamba <juan.antamba@udla.edu.ec>
2023-12-03 03:33:04 -0500

    Se crea la funcion para que se pueda crear la matriz de unos

--- MatrizTridimencional.c
+++ MatrizTridimencional.c
@@ -7,3 +7,14 @@ void inicializarMatrizCeros(int filas, int columnas, int matriz[filas][columnas]
         }
     }
 }
+
+// Se crea la funcion para que se cree la matriz de unos
+void inicializarUltimaMatrizEnUnos(int num_matrices, int filas, int columnas, int matriz[num_matrices][filas][columnas]) {
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            matriz[num_matrices - 1][i][j] = 1;
+        }
+    }
+}
+
+
